1940f926dcde0291c8443a2319104afcf9fcc235
Mark Hamstra <hello@markhamstra.com>
2012-12-08 23:42:46 +0100

    Add settings/remove processor

---
+++ core/components/clientconfig/processors/mgr/settings/remove.class.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package ClientConfig
+ */
+class cgSettingRemoveProcessor extends modObjectRemoveProcessor {
+    public $classKey = 'cgSetting';
+    public $objectType = 'cgSetting';
+}
+return 'cgSettingRemoveProcessor';
