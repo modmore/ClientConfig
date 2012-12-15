f0b25372d4ec8b2342bc37f931d312dae4097d9a
Mark Hamstra <hello@markhamstra.com>
2012-12-15 05:36:59 +0100

    Improve checking if key exists on updating a setting. // #30  Fix bug with incorrectly checking cgSetting.is_required checkbox

--- core/components/clientconfig/processors/mgr/settings/create.class.php
+++ core/components/clientconfig/processors/mgr/settings/create.class.php
@@ -7,18 +7,21 @@ class cgSettingCreateProcessor extends modObjectCreateProcessor {
     public $languageTopics = array('clientconfig:default');
 
     /**
-     * Before setting, we check if the name is filled and/or already exists.
+     * Before setting, we check if the name is filled and/or already exists. Also checkboxes.
      * @return bool
      */
     public function beforeSet() {
         $key = $this->getProperty('key');
         if (empty($key)) {
-            $this->addFieldError('key',$this->modx->lexicon('clientconfig.clientconfigmenu_err_ns_key'));
+            $this->addFieldError('key',$this->modx->lexicon('clientconfig.cgsetting_err_ns_key'));
         } else {
             if ($this->doesAlreadyExist(array('key' => $key))) {
-                $this->addFieldError('key',$this->modx->lexicon('clientconfig.clientconfigmenu_err_ae'));
+                $this->addFieldError('key',$this->modx->lexicon('clientconfig.cgsetting_err_ae_key'));
             }
         }
+
+        /* Set is_required checkbox */
+        $this->setCheckbox('is_required', true);
         return parent::beforeSet();
     }
 }
--- core/components/clientconfig/processors/mgr/settings/update.class.php
+++ core/components/clientconfig/processors/mgr/settings/update.class.php
@@ -5,14 +5,23 @@
 class cgSettingUpdateProcessor extends modObjectUpdateProcessor {
     public $classKey = 'cgSetting';
     public $languageTopics = array('clientconfig:default');
-    public $objectType = 'clientconfig.clientconfigmenu';
 
     /**
-     * Validate stuff before save.
+     * Before setting, we check if the name is filled and/or already exists. Also checkboxes.
      * @return bool
      */
-    public function beforeSave() {
-        return parent::beforeSave();
+    public function beforeSet() {
+        $key = $this->getProperty('key');
+        if (empty($key)) {
+            $this->addFieldError('key',$this->modx->lexicon('clientconfig.cgsetting_err_ns_key'));
+        } else {
+            if ($this->modx->getCount($this->classKey, array('key' => $key, 'AND:id:!=' => $this->object->get('id'))) > 0) {
+                $this->addFieldError('key',$this->modx->lexicon('clientconfig.cgsetting_err_ae_key'));
+            }
+        }
+
+        $this->setCheckbox('is_required', true);
+        return parent::beforeSet();
     }
 }
 return 'cgSettingUpdateProcessor';
