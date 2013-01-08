74c35363e2745e91bf490083c4f1abb22fd65f6e
Ivan Klimchuk <alroniks@gmail.com>
2013-01-08 10:02:40 +0300

    Added russian lexicon

---
+++ core/components/clientconfig/lexicon/ru/default.inc.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * ClientConfig
+ *
+ * Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
+ *
+ * ClientConfig is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * ClientConfig is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ClientConfig; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package clientconfig
+ * @subpackage lexicon-ru
+ * @language ru
+ * @author Ivan Klimchuk (Alroniks, ivan@klimchuk.com)
+ * @date 2013-01-08
+ */
+
+$_lang['clientconfig'] = 'Конфигурация';
+$_lang['clientconfig.desc'] = 'Установка и обновление конфигурации сайта.';
+$_lang['clientconfig.add_setting'] = 'Добавить настройку';
+$_lang['clientconfig.update_setting'] = 'Обновить настройку';
+$_lang['clientconfig.remove_setting'] = 'Удалить настройку';
+$_lang['clientconfig.remove_setting.confirm'] = 'Вы уверены, что хотите удалить этот параметр?';
+$_lang['clientconfig.add_group'] = 'Добавить группу';
+$_lang['clientconfig.update_group'] = 'Обновить группу';
+$_lang['clientconfig.remove_group'] = 'Удалить группу';
+$_lang['clientconfig.remove_group.confirm'] = 'Вы уверены, что хотите удалить эту группу? Все параметры из этой группы не будут видны для клиента, пока вы не добавите их в другую группу.';
+$_lang['clientconfig.admin'] = 'Администрирование';
+$_lang['clientconfig.adminpanel'] = 'Панель администрирования конфигурации';
+$_lang['clientconfig.cgsetting_err_ns_key'] = 'Обязателен ключ для настройки.';
+$_lang['clientconfig.cgsetting_err_ae_key'] = 'Такой ключ уже существует.';
+$_lang['clientconfig.description'] = 'Описание';
+$_lang['clientconfig.default'] = 'Значение по умолчанию';
+$_lang['clientconfig.error.noresults'] = 'Элементы не найдены.';
+$_lang['clientconfig.filter_on_group'] = 'Фильтр по группе';
+$_lang['clientconfig.id'] = 'ID';
+$_lang['clientconfig.is_required'] = 'Обязателен?';
+$_lang['clientconfig.is_required.long'] = 'Настройка обязательна';
+$_lang['clientconfig.group'] = 'Группа';
+$_lang['clientconfig.groups'] = 'Группы';
+$_lang['clientconfig.key'] = 'Ключ';
+$_lang['clientconfig.label'] = 'Название';
+$_lang['clientconfig.no_configuration_yet'] = 'Нет доступных конфигураций';
+$_lang['clientconfig.no_configuration_yet.desc'] = 'Кажется, еще не созданы какие-либо конфигурации. Если вы являетесь администратором сайта, пожалуйста изучите <a href="http://rtfm.modx.com/display/ADDON/ClientConfig">официальную документацию</a> по настройке конфигурации для вашего клиента.';
+$_lang['clientconfig.options'] = 'Параметры поля';
+$_lang['clientconfig.options.desc'] = 'Для некоторых типов полей, таких как выпадающие списки, вы можете задать параметры. Разделите разные параметры двумя вертикальными чертами (||) и если вы хотите другое значение, чем то, что видит клиент, используейте "название==значение".';
+$_lang['clientconfig.save_config'] = 'Сохранить конфигурацию';
+$_lang['clientconfig.sortorder'] = 'Порядок сортировки';
+$_lang['clientconfig.settings'] = 'Настройки';
+$_lang['clientconfig.settings_count'] = 'Кол-во настроек';
+$_lang['clientconfig.value'] = 'Значение';
+$_lang['clientconfig.xtype'] = 'Тип поля';
+$_lang['clientconfig.xtype.textfield'] = 'Текст';
+$_lang['clientconfig.xtype.textarea'] = 'Текстовая область';
+$_lang['clientconfig.xtype.numberfield'] = 'Число';
+$_lang['clientconfig.xtype.colorpalette'] = 'Выбор цвета';
+$_lang['clientconfig.xtype.xcheckbox'] = 'Чекбокс';
+$_lang['clientconfig.xtype.datefield'] = 'Дата';
+$_lang['clientconfig.xtype.timefield'] = 'Время';
+$_lang['clientconfig.xtype.combobox'] = 'Выпадающий список';
+$_lang['clientconfig.to_client_view'] = 'Показать клиенту';
+$_lang['clientconfig.saved'] = 'Сохранено';
+$_lang['clientconfig.saved.text'] = 'Настройки были сохранены.';
+$_lang['clientconfig.field_is_required'] = 'Этот параметр не может быть пустым.';
