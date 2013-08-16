c55ffdd5869d4f6a82e9245fc5517686a998af5f
Mark Hamstra <hello@markhamstra.com>
2013-08-16 22:05:19 +0200

    Don't version config.core.php but provide sample

--- config.core.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('MODX_CORE_PATH', dirname(dirname(__FILE__)).'/modx-2.2.6-pl/core/');
-define('MODX_CONFIG_KEY', 'config');
-?> 
---
+++ config.core.sample.php
@@ -0,0 +1,3 @@
+<?php
+define('MODX_CORE_PATH', dirname(dirname(__FILE__)).'/modx/core/');
+define('MODX_CONFIG_KEY', 'config');
