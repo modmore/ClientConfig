2d4c9543cafb2869c0a73ec83e91981c8dbca96d
sepiariver <info@sepiariver.com>
2013-09-21 16:54:37 -0700

    modified cgSettings to leverage the new class function

--- core/components/clientconfig/elements/snippets/cgSettings.snippet.php
+++ core/components/clientconfig/elements/snippets/cgSettings.snippet.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/* 
  * cgSettings
  *
  * Returns ClientConfig settings. 
@@ -7,29 +7,30 @@
  * Output can be formatted with a tpl chunk, otherwise an array of results is printed.
  * Example usage: [[cgSettings?group=`2`&tpl=`myTpl`]]
  * 
- * TODO: require the class file and call ClientConfig.getSettings() instead of duplicating function here.
-**/
- 
-/* Set defaults */
+ */
+
+/* Get properties */
+$key = $modx->getOption('key',$scriptProperties,'');
 $group = $modx->getOption('group',$scriptProperties,'');
-$groupSet = ($group) ? array('group'=>$group) : null;
 $tpl = $modx->getOption('tpl',$scriptProperties,'');
 
-/* Attempt to get settings from cache if no group filter */
-if (!$group) {
-    $cacheOptions = array(xPDO::OPT_CACHE_KEY => 'system_settings');
-    $settings = $modx->getCacheManager()->get('clientconfig', $cacheOptions);
-}
+/* Grab the class */
+$path = $modx->getOption('clientconfig.core_path', null, $modx->getOption('core_path') . 'components/clientconfig/');
+$path .= 'model/clientconfig/';
+$clientConfig = $modx->getService('clientconfig','ClientConfig', $path);
+ 
+/* Set filters */
+$where = array();
+if ($key) $where['key'] = $key; 
+if ($group) $where['group'] = $group;
 
-/* Otherwise query the database with optional filter */
-if (empty($settings) && $modx->getCount('cgSetting') > 0) {
-    $collection = $modx->getCollection('cgSetting', $groupSet);
-    $settings = array();
-    /* @var cgSetting $setting */
-    foreach ($collection as $setting) {
-        $settings[$setting->get('key')] = $setting->get('value');
-    }
-}
+/* Set cache id */
+$cacheId = 'clientconfig';
+if ($group) $cacheId = 'clientconfig.group.' . $group;
+if ($key) $cacheId = 'clientconfig.key.' . $key;
+
+/* If we got the class (gotta be careful of failed migrations), grab settings and go! */
+if ($clientConfig instanceof ClientConfig) $settings = $clientConfig->getSettings($where, $cacheId);
 
 /* Format the output */
 if (!$tpl) return print_r($settings);
