844ffc9d3ee8082a258d9dc5c2e919a7d5c2ee10
Mark Hamstra <hello@markhamstra.com>
2015-12-09 17:08:38 +0100

    1.3.2

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME','ClientConfig');
     define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-    define('PKG_VERSION','1.3.1');
+    define('PKG_VERSION','1.3.2');
     define('PKG_RELEASE','pl');
 
     /* load modx */
