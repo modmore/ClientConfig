0f45a16410bab421726ccc4c40f2807fb315ff77
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2016-08-21 18:30:40 +0200

    Create updatefromgrid.class.php
    Deals with updated values from the grid.

---
+++ core/components/clientconfig/processors/mgr/settings/updatefromgrid.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Updates a cgSetting object from the grid
+ */
+require_once (dirname(__FILE__).'/update.class.php');
+
+class cgSettingUpdateFromGridProcessor extends cgSettingUpdateProcessor {
+    public function initialize() {
+        $data = $this->getProperty('data');
+        if (empty($data)) return $this->modx->lexicon('invalid_data');
+        $data = $this->modx->fromJSON($data);
+        if (empty($data)) return $this->modx->lexicon('invalid_data');
+        $this->setProperties($data);
+        $this->unsetProperty('data');
+        return parent::initialize();
+    }
+}
+
+return 'cgSettingUpdateFromGridProcessor';
