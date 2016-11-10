8d849f153bfa312a92c447c06e999ffa35ebe5cb
Pavel <daruse93@gmail.com>
2016-11-10 19:03:08 +0300

    create Event «OnConfigChange»

---
+++ _build/resolvers/events.resolver.php
@@ -0,0 +1,20 @@
+<?php
+/* @var modX $modx */
+
+if ($object->xpdo) {
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_UPGRADE:
+        case xPDOTransport::ACTION_INSTALL:
+            $modx =& $object->xpdo;
+            /* Create Event*/
+            $Event = $modx->newObject('modEvent');
+            $Event->set('name', 'OnConfigChange');
+            $Event->set('service',1); 
+            $Event->set('groupname', 'ClientConfig');
+            $Event->save();
+        break;
+    }
+
+}
+return true;
+
