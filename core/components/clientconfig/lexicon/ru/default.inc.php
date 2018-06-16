fdd1dc3bd7c1a65595d044653c309a33764c4d30
Ruslan Aleev <aleev.insmile@gmail.com>
2018-06-17 00:16:58 +0400

    Added missing russian translation
    Added missing items of Russian translation

--- core/components/clientconfig/lexicon/ru/default.inc.php
+++ core/components/clientconfig/lexicon/ru/default.inc.php
@@ -20,7 +20,7 @@
  * @package clientconfig
  * @subpackage lexicon-ru
  * @language ru
- * @author Ivan Klimchuk (Alroniks, ivan@klimchuk.com)
+ * @author Ivan Klimchuk (Alroniks, ivan@klimchuk.com), Ruslan Aleev (info@cat-art.ru)
  * @date 2013-01-08
  */
 
@@ -30,7 +30,7 @@ $_lang['clientconfig.add_setting'] = 'Добавить настройку';
 $_lang['clientconfig.update_setting'] = 'Обновить настройку';
 $_lang['clientconfig.duplicate_setting'] = 'Копировать настройку';
 $_lang['clientconfig.remove_setting'] = 'Удалить настройку';
-$_lang['clientconfig.remove_setting.confirm'] = 'Вы уверены, что хотите удалить этот параметр?';
+$_lang['clientconfig.remove_setting.confirm'] = 'Вы уверены, что хотите удалить эту настройку?';
 $_lang['clientconfig.add_group'] = 'Добавить группу';
 $_lang['clientconfig.update_group'] = 'Обновить группу';
 $_lang['clientconfig.remove_group'] = 'Удалить группу';
@@ -53,7 +53,7 @@ $_lang['clientconfig.label'] = 'Название';
 $_lang['clientconfig.no_configuration_yet'] = 'Нет доступных конфигураций';
 $_lang['clientconfig.no_configuration_yet.desc'] = 'Кажется, еще не созданы какие-либо конфигурации. Если вы являетесь администратором сайта, пожалуйста изучите <a href="http://rtfm.modx.com/display/ADDON/ClientConfig">официальную документацию</a> по настройке конфигурации для вашего клиента.';
 $_lang['clientconfig.options'] = 'Параметры поля';
-$_lang['clientconfig.options.desc'] = 'Для некоторых типов полей, таких как выпадающие списки, вы можете задать параметры. Разделите разные параметры двумя вертикальными чертами (||) и если вы хотите другое значение, чем то, что видит клиент, используейте "название==значение".';
+$_lang['clientconfig.options.desc'] = 'Для некоторых типов полей, таких как выпадающие списки, вы можете задать параметры. Разделите разные параметры двумя вертикальными чертами (||) и если вы хотите другое значение, чем то, что видит клиент, используйте "название==значение".';
 $_lang['clientconfig.save_config'] = 'Сохранить конфигурацию';
 $_lang['clientconfig.sortorder'] = 'Порядок сортировки';
 $_lang['clientconfig.settings'] = 'Настройки';
@@ -62,13 +62,46 @@ $_lang['clientconfig.value'] = 'Значение';
 $_lang['clientconfig.xtype'] = 'Тип поля';
 $_lang['clientconfig.xtype.textfield'] = 'Текст';
 $_lang['clientconfig.xtype.textarea'] = 'Текстовая область';
+$_lang['clientconfig.xtype.richtext'] = 'Текстовый редактор';
 $_lang['clientconfig.xtype.numberfield'] = 'Число';
 $_lang['clientconfig.xtype.colorpalette'] = 'Выбор цвета';
 $_lang['clientconfig.xtype.xcheckbox'] = 'Чекбокс';
 $_lang['clientconfig.xtype.datefield'] = 'Дата';
 $_lang['clientconfig.xtype.timefield'] = 'Время';
 $_lang['clientconfig.xtype.combobox'] = 'Выпадающий список';
+$_lang['clientconfig.xtype.image'] = 'Изображение';
+$_lang['clientconfig.xtype.googlefonts'] = 'Список шрифтов Google';
 $_lang['clientconfig.to_client_view'] = 'Показать клиенту';
 $_lang['clientconfig.saved'] = 'Сохранено';
 $_lang['clientconfig.saved.text'] = 'Настройки были сохранены.';
 $_lang['clientconfig.field_is_required'] = 'Этот параметр не может быть пустым.';
+
+// New 2014/07/20
+$_lang['clientconfig.create_groups_first'] = 'Сначала создайте группу';
+$_lang['clientconfig.create_groups_first.desc'] = 'Извините, но прежде чем вы сможете добавить настройку, вам необходима хотя бы одна группа. Без группы настройки не могут быть отображены клиенту.';
+
+$_lang['clientconfig.export_settings'] = 'Экспортировать настройки';
+$_lang['clientconfig.export_settings.confirm'] = 'Когда вы нажмете «Да», будет сгенерирован и скачан XML-файл, который содержит все настройки ClientConfig. Вы уверены что хотите продолжить?';
+$_lang['clientconfig.import_settings'] = 'Импортировать настройки';
+$_lang['clientconfig.import_settings.desc'] = 'Загрузив XML-файл и выбрав правильный режим импорта, вы можете импортировать настройки, экспортированные ранее или с другого сайта. <b>Примечание:</b> настройки содержат ссылки на группы по их ID; если вы импортируете настройки, вам потребуется также импортировать и группы.';
+
+$_lang['clientconfig.export_groups'] = 'Экспортировать группы';
+$_lang['clientconfig.export_groups.confirm'] = 'Когда вы нажмете «Да», будет сгенерирован и скачан XML-файл, который содержит все группы ClientConfig. Вы уверены что хотите продолжить?';
+$_lang['clientconfig.import_groups'] = 'Импортировать группы';
+$_lang['clientconfig.import_groups.desc'] = 'Загрузив XML-файл и выбрав правильный режим импорта, вы можете импортировать группы, экспортированные ранее или с другого сайта.';
+
+$_lang['clientconfig.import_file'] = 'Файл для импорта';
+$_lang['clientconfig.import_mode'] = 'Режим импорта';
+$_lang['clientconfig.import_mode.insert'] = "Вставить: оставить существующие [[+what]] и добавить импортированные данные";
+$_lang['clientconfig.import_mode.overwrite'] = "Перезаписать: оставить существующие [[+what]], но перезаписать их, если они имеют одинаковый ID";
+$_lang['clientconfig.import_mode.replace'] = "Заменить: сначала удалить все текущие [[+what]], а затем импортировать новые данные";
+$_lang['clientconfig.start_import'] = 'Начать импорт';
+$_lang['clientconfig.error.xml_not_loaded'] = 'Не загружен корректный XML-файл.';
+$_lang['clientconfig.error.not_an_export'] = 'Загруженный файл не является корректным файлом экспорта для ClientConfig.';
+$_lang['clientconfig.error.importing_row'] = 'Что-то пошло не так в процессе сохранения экспорта: ';
+
+// New 2017-01-31
+$_lang['clientconfig.xtype.password'] = 'Пароль';
+$_lang['clientconfig.xtype.file'] = 'Файл';
+$_lang['clientconfig.source'] = 'Источник файлов';
+$_lang['clientconfig.source.desc'] = 'Источник файлов для диспетчера файлов.';
