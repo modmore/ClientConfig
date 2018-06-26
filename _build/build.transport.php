410a61fc58d5b6e0e83082c5738d503763d70854
Mark Hamstra <hello@markhamstra.com>
2018-06-26 22:45:16 +0200

    2.0.0-pl

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -24,7 +24,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     define('PKG_NAME','ClientConfig');
     define('PKG_NAME_LOWER',strtolower(PKG_NAME));
     define('PKG_VERSION','2.0.0');
-    define('PKG_RELEASE','rc1');
+    define('PKG_RELEASE','pl');
 
     /* load modx */
     require_once dirname(dirname(__FILE__)) . '/config.core.php';
