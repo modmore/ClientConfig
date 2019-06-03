2318038baa34efbe65d941a6a0727cb3f5aaf555
Gerben van de Kraats <gerben@census.nl>
2019-06-03 14:19:52 +0200

    #152  - Check if value is empty  - Fallback to system settings

--- core/components/clientconfig/model/clientconfig/cgsetting.class.php
+++ core/components/clientconfig/model/clientconfig/cgsetting.class.php
@@ -5,14 +5,17 @@
 class cgSetting extends xPDOSimpleObject {
     public function prefixSourceUrl($value)
     {
-        $sourceId = $this->get('source');
-        $this->xpdo->loadClass('sources.modMediaSource');
-        $source = modMediaSource::getDefaultSource($this->xpdo, $sourceId);
-        if (!$source) {
-            return $value;
+        if($value !== '') {
+            $sourceId = $this->get('source');
+            $this->xpdo->loadClass('sources.modMediaSource');
+            $source = modMediaSource::getDefaultSource($this->xpdo, $sourceId);
+            if (!$source) {
+                return $value;
+            }
+            $source->getWorkingContext();
+            $source->initialize();
+            return $source->getObjectUrl($value);
         }
-        $source->getWorkingContext();
-        $source->initialize();
-        return $source->getObjectUrl($value);
+        return $this->xpdo->getOption($this->key);
     }
 }
