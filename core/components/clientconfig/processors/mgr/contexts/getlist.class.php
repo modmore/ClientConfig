abe1c457d16d4e0ad89b3e11fd79fcf788f8a8be
digitalpenguin <murray@digitalpenguin.hk>
2022-06-11 08:53:16 +0800

    Fix cgContextsGetListProcessor on MODX 3

--- core/components/clientconfig/processors/mgr/contexts/getlist.class.php
+++ core/components/clientconfig/processors/mgr/contexts/getlist.class.php
@@ -1,8 +1,27 @@
 <?php
-require_once MODX_CORE_PATH . 'model/modx/processors/context/getlist.class.php';
 
-class cgContextsGetlistProcessor extends modContextGetListProcessor
+use MODX\Revolution\Processors\Context\GetList;
+
+// Use old 2.x version if not on MODX 3+
+if (!class_exists(GetList::class)) {
+    require_once MODX_CORE_PATH . 'model/modx/processors/context/getlist.class.php';
+    
+    class cgContextsGetlistProcessor extends modContextGetListProcessor
+    {
+        use GetContexts;
+    }
+    return 'cgContextsGetlistProcessor';
+}
+
+// 3.x version
+class cgContextsGetlistProcessor extends GetList
 {
+    use GetContexts;
+}
+return 'cgContextsGetlistProcessor';
+
+// Common trait for both versions of processor
+trait GetContexts {
     public function beforeIteration(array $list)
     {
         $list[] = [
@@ -12,5 +31,3 @@ class cgContextsGetlistProcessor extends modContextGetListProcessor
         return $list;
     }
 }
-
-return 'cgContextsGetlistProcessor';
