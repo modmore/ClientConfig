2ebfd7be0a6e56042fa32b69078bbea612df6f6d
Mark Hamstra <hello@markhamstra.com>
2023-11-07 15:02:28 +0100

    Prepare 2.4 release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME','ClientConfig');
     define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-    define('PKG_VERSION','2.3.3');
+    define('PKG_VERSION','2.4.0');
     define('PKG_RELEASE','pl');
 
     /* load modx */
